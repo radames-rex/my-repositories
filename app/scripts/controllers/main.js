b1945387feff11634117bc2456540f6b1cfb9a35
radames-rex <radames.rex@gmail.com>
2016-12-06 17:25:30 -0300

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myRepositoriesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myRepositoriesApp
+ */
+angular.module('myRepositoriesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
