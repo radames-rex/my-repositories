a7e6f51e708a487ede93f811be122bc6bef0b003
radames <radames@infoway-pi.com.br>
2016-12-07 15:52:35 -0300

    Alteração na estrutura do scaffold

---
+++ app/components/repositories/repositories.factory.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myRepositoriesApp.factory:RepositoriesFactory
+ * @description
+ * # RepositoriesFactory
+ * Factory of the myRepositoriesApp
+ */
+angular.module('myRepositoriesApp')
+  .factory('RepositoriesFactory', function ($scope) {
+
+    RepositoriesFactory.getRepositories = function(){
+
+    }
+
+    RepositoriesFactory.filterRepositories = function(){
+
+    }
+
+  });
